0bc2911f24426b9ef053c7c9ddad6d325b06ce84
InvestLab <nextsec.vn@gmail.com>
2023-11-08 15:17:25 +0700

    Update auto-deploy-modsec.sh

--- auto-deploy-modsec.sh
+++ auto-deploy-modsec.sh
@@ -94,7 +94,7 @@ version: '3'
 
 services:
    modsec:
-     image: "wisoez/nginx-modsecurity-sticky"
+     image: "wisoez/nginx-sticky-alpine"
      volumes:
        - "/data/nginx/conf.d:/etc/nginx/conf.d"
        - "/data/nginx/nginx.conf:/etc/nginx/nginx.conf"
